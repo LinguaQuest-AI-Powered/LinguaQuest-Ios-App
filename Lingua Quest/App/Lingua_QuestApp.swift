1881f40819fa537035aa0a112fb19cf47c2c4f36
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-07-14 15:01:47 +0300

    refactor: implement app configuration management using xcconfig files and clean up default SwiftData boilerplate

---
+++ Lingua Quest/App/Lingua_QuestApp.swift
@@ -0,0 +1,21 @@
+//
+//  Lingua_QuestApp.swift
+//  Lingua Quest
+//
+//  Created by siam on 14/07/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Lingua_QuestApp: App {
+   
+
+    var body: some Scene {
+        WindowGroup {
+        
+        }
+
+    }
+}
